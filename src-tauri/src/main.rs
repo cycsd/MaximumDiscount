bd8fc36380e408d8e637bf13d161b3646f5f0d02
cycsd <cycsd9211@gmail.com>
2023-07-08 17:42:37 +0800

    add tailwind

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,12 +4,16 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    format!("Hello Hey, {}! You've been greeted from Rust!", name)
+}
+#[tauri::command]
+fn max_dicount(_name: &str)->String{
+    format!("Hello Gray")
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet,max_dicount])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,67 @@
-import { createSignal } from "solid-js";
+import { For, createSignal } from "solid-js";
+import { createStore } from "solid-js/store";
 import logo from "./assets/logo.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import { create } from "domain";
 
+interface Merchandise {
+  name: string,
+  price: number,
+  amount: number,
+}
 function App() {
   const [greetMsg, setGreetMsg] = createSignal("");
+  const [candidateMerchandise, setcandidateMerchandise] = createSignal<Merchandise[]>([]);
   const [name, setName] = createSignal("");
+  const [price, setPrice] = createSignal(0);
+  const [amount, setAmount] = createSignal(0);
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name: name() }));
   }
+  const enterMerchandise = () => {
+    const newMerchandise: Merchandise = {
+      name: name(),
+      price: price(),
+      amount: amount(),
+    }
+    setcandidateMerchandise(old => [...old,newMerchandise])
+  }
 
   return (
-    <div class="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div class="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={logo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
-
-      <form
-        class="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
+    <>
+      <h1>Max Your Discount!</h1>
+      <div class="flex justify-center">
         <input
-          id="greet-input"
           onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+          placeholder="Enter merchandise name..."
         />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg()}</p>
-    </div>
+        <input
+          onChange={(e) => setPrice(parseInt(e.currentTarget.value))}
+          placeholder="Enter merchandise amount..."
+        />
+        <input
+          onChange={(e) => setAmount(parseInt(e.currentTarget.value))}
+          placeholder="Enter merchandise price..."
+        />
+        <button onClick={enterMerchandise} type="button">Enter</button>
+        <button onClick={greet} type="submit">Greet</button>
+        <p>{greetMsg()}</p>
+      </div>
+      <div class="flex justify-center">
+        <For each={candidateMerchandise()}>
+          {(merchandise) => (<div>
+            <p>{merchandise.name}</p>
+            <p>{merchandise.price}</p>
+            <p>{merchandise.amount}</p>
+          </div>
+          )
+          }
+        </For>
+      </div>
+    </>
   );
 }
 
