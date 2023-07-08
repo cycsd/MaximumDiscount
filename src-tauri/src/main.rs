7c4915e1225c56b04f7ccfdf1b329838f4b732cf
cycsd <cycsd9211@gmail.com>
2023-07-09 00:35:39 +0800

    測試呼叫rust函數

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,48 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
+use serde::{Serialize,Deserialize};
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello Hey, {}! You've been greeted from Rust!", name)
 }
+#[derive(Deserialize,Serialize,Debug,Default)]
+struct Merchandise{
+    name:String,
+    price:i32,
+    amount:i32,
+}
+#[derive(Deserialize,Serialize,Debug,Default)]
+struct MerchandiseDTO{
+    merchandises:Vec<Merchandise>,
+    minum_threshold:f64,
+}
+trait Caculator {
+    fn caculate_discount(&self)->();
+}
+impl Caculator for Vec<Merchandise>{
+    fn caculate_discount(&self)->() {
+        let price:Vec<i32> = self.iter().map(|m|m.price).collect();
+        let seed = vec![0;self.len()];
+        let mut stack = vec![seed];
+        while let Some(quantitiy_candidate) = stack.pop() {
+            
+        }
+    }
+}
 #[tauri::command]
-fn max_dicount(_name: &str)->String{
-    format!("Hello Gray")
+fn max_discount(merchandise_dto:MerchandiseDTO)->Vec<Merchandise>{
+    println!("hi");
+    println!("threshold:{}",merchandise_dto.minum_threshold);
+
+    //format!("first_merchandise: {:?}",merchandises[1])
+    //merchandises_info.merchandises
+    merchandise_dto.merchandises
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet,max_dicount])
+        .invoke_handler(tauri::generate_handler![greet,max_discount])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { For, createSignal } from "solid-js";
+import { For, createResource, createSignal } from "solid-js";
 import { createStore } from "solid-js/store";
 import logo from "./assets/logo.svg";
 import { invoke } from "@tauri-apps/api/tauri";
@@ -10,24 +10,54 @@ interface Merchandise {
   price: number,
   amount: number,
 }
+interface MerchandiseDTO {
+  merchandises: Merchandise[],
+  minum_threshold: number,
+}
 function App() {
+  let formData = {
+    name: document.createElement('input'),
+    price: document.createElement('input'),
+    amount: document.createElement('input'),
+    minimum_threshold_price: document.createElement('input'),
+    discount: document.createElement('input'),
+  };
   const [greetMsg, setGreetMsg] = createSignal("");
-  const [candidateMerchandise, setcandidateMerchandise] = createSignal<Merchandise[]>([]);
+  const [candidateMerchandise, setcandidateMerchandise] = createStore<Merchandise[]>([]);
   const [name, setName] = createSignal("");
-  const [price, setPrice] = createSignal(0);
-  const [amount, setAmount] = createSignal(0);
+  const [merchandise, setmerchandise] = createStore<Merchandise[]>([]);
+
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name: name() }));
   }
-  const enterMerchandise = () => {
-    const newMerchandise: Merchandise = {
-      name: name(),
-      price: price(),
-      amount: amount(),
+
+  const caculateDiscount = async () => {
+    const dto: MerchandiseDTO = {
+      merchandises: candidateMerchandise,
+      minum_threshold:parseFloat(formData.minimum_threshold_price.value),
     }
-    setcandidateMerchandise(old => [...old,newMerchandise])
+    const mer = await invoke<Merchandise[]>("max_discount", { merchandiseDto: dto });
+    setmerchandise(mer);
+    return mer;
+  }
+
+
+  const getMerchandiseInfo = () => {
+    return {
+      name: formData.name.value,
+      price: parseInt(formData.price.value),
+      amount: parseInt(formData.amount.value),
+    }
+  }
+  const enterMerchandise = () => {
+    // const newMerchandise: Merchandise = {
+    //   name: name(),
+    //   price: price(),
+    //   amount: amount(),
+    // }
+    setcandidateMerchandise(old => [...old, getMerchandiseInfo()])
   }
 
   return (
@@ -35,24 +65,39 @@ function App() {
       <h1>Max Your Discount!</h1>
       <div class="flex justify-center">
         <input
-          onChange={(e) => setName(e.currentTarget.value)}
+          ref={formData!.name}
+          //onChange={(e) => setName(e.currentTarget.value)}
           placeholder="Enter merchandise name..."
         />
         <input
-          onChange={(e) => setPrice(parseInt(e.currentTarget.value))}
+          ref={formData!.price}
+          //onChange={(e) => setPrice(parseInt(e.currentTarget.value))}
           placeholder="Enter merchandise amount..."
         />
         <input
-          onChange={(e) => setAmount(parseInt(e.currentTarget.value))}
+          ref={formData!.amount}
+          //onChange={(e) => setAmount(parseInt(e.currentTarget.value))}
           placeholder="Enter merchandise price..."
         />
+
         <button onClick={enterMerchandise} type="button">Enter</button>
-        <button onClick={greet} type="submit">Greet</button>
-        <p>{greetMsg()}</p>
+        <button onClick={caculateDiscount} type="button">Caculate</button>
+
       </div>
       <div class="flex justify-center">
-        <For each={candidateMerchandise()}>
-          {(merchandise) => (<div>
+      <input
+          ref={formData!.minimum_threshold_price}
+          placeholder="輸入最低限度金額..."
+        />
+        <input
+          ref={formData!.discount}
+          placeholder="輸入折扣金額..."
+        />
+      </div>
+      <div class="bg-red-500"><p>i am here{formData.name.value}</p></div>
+      <div class="flex justify-center flex-wrap">
+        <For each={candidateMerchandise}>
+          {(merchandise) => (<div class="grid grid-cols-3 gap-4 w-3/4">
             <p>{merchandise.name}</p>
             <p>{merchandise.price}</p>
             <p>{merchandise.amount}</p>
@@ -61,6 +106,15 @@ function App() {
           }
         </For>
       </div>
+      <For each={merchandise}>
+        {(merchandise) => (<div class="grid grid-cols-3 gap-4 w-3/4">
+          <p>{merchandise.name}</p>
+          <p>{merchandise.price}</p>
+          <p>{merchandise.amount}</p>
+        </div>
+        )
+        }
+      </For>
     </>
   );
 }
