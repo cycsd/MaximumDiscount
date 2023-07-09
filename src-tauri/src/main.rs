9a7d273b45738db231286e1c98911672b75ae2cb
cycsd <cycsd9211@gmail.com>
2023-07-10 00:52:21 +0800

    基本架構完成

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,48 +1,111 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use serde::{Serialize,Deserialize};
+use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello Hey, {}! You've been greeted from Rust!", name)
+
+type Quantities = Vec<i32>;
+type Price = f64;
+#[derive(Deserialize, Serialize, Debug, Default)]
+struct Merchandise {
+    name: String,
+    price: Price,
+    amount: i32,
 }
-#[derive(Deserialize,Serialize,Debug,Default)]
-struct Merchandise{
-    name:String,
-    price:i32,
-    amount:i32,
+#[derive(Deserialize, Serialize, Debug, Default)]
+struct MerchandiseDTO {
+    merchandises: Vec<Merchandise>,
+    minimum_threshold: Price,
+    discount: Price,
 }
-#[derive(Deserialize,Serialize,Debug,Default)]
-struct MerchandiseDTO{
-    merchandises:Vec<Merchandise>,
-    minum_threshold:f64,
+#[derive(Deserialize, Serialize, Debug, Default)]
+struct Combine {
+    total_amount:i32,
+    price: Price,
+    discount_price: Price,
+    quantities_combine: Quantities,
+    cost_performance_ratio: f64,
+    cost_per_unit: f64,
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+struct CombineList {
+    merchandises: Vec<Merchandise>,
+    combines: Vec<Combine>,
+    minimum_threshold: Price,
+    discount: Price,
 }
 trait Caculator {
-    fn caculate_discount(&self)->();
+    fn caculate_discount(&self, minimum_threshold: f64, discount: f64) -> Vec<Combine>;
 }
-impl Caculator for Vec<Merchandise>{
-    fn caculate_discount(&self)->() {
-        let price:Vec<i32> = self.iter().map(|m|m.price).collect();
-        let seed = vec![0;self.len()];
-        let mut stack = vec![seed];
-        while let Some(quantitiy_candidate) = stack.pop() {
-            
+impl Caculator for Vec<Merchandise> {
+    fn caculate_discount(&self, minimum_threshold: f64, discount: f64) -> Vec<Combine> {
+        let seed: (Quantities, Price) = (vec![0; self.len()], 0.0);
+        let mut stack: Vec<(Quantities, Price)> = vec![seed];
+        let mut visited: HashSet<Quantities> = HashSet::new();
+        let mut result: Vec<(Quantities, Price)> = vec![];
+        while let Some((mut quantitiy_candidate, price)) = stack.pop() {
+            if price > minimum_threshold {
+                result.push((quantitiy_candidate, price));
+                continue;
+            }
+            (0..self.len()).for_each(|index| {
+                quantitiy_candidate[index] += 1;
+                if !visited.contains(&quantitiy_candidate) {
+                    let merchandise = &self[index];
+                    let add_price = merchandise.price;
+                    let new_candidate = quantitiy_candidate.clone();
+                    stack.push((new_candidate, price + add_price));
+                    visited.insert(quantitiy_candidate.clone());
+                }
+                quantitiy_candidate[index] -= 1;
+            })
         }
+        result
+            .into_iter()
+            .map(|(quantities, price)| {
+                let total_amount = (0..self.len())
+                    .map(|index| {
+                        let amount = self[index].amount;
+                        let quantity = quantities[index];
+                        amount * quantity
+                    })
+                    .sum::<i32>() as f64;
+                let discount_price = price - discount;
+                Combine {
+                    total_amount:total_amount as i32,
+                    price,
+                    discount_price,
+                    quantities_combine: quantities,
+                    cost_per_unit: discount_price / total_amount,
+                    cost_performance_ratio: total_amount / discount_price,
+                }
+            })
+            .collect()
     }
 }
 #[tauri::command]
-fn max_discount(merchandise_dto:MerchandiseDTO)->Vec<Merchandise>{
-    println!("hi");
-    println!("threshold:{}",merchandise_dto.minum_threshold);
+fn max_discount(merchandise_dto: MerchandiseDTO) -> CombineList {
+    let (merchandises, minimum_threshold, discount) = (
+        merchandise_dto.merchandises,
+        merchandise_dto.minimum_threshold,
+        merchandise_dto.discount,
+    );
+    let mut combines = merchandises.caculate_discount(minimum_threshold, discount);
+    combines.sort_by(|a, b| a.cost_per_unit.partial_cmp(&b.cost_per_unit).unwrap());
 
-    //format!("first_merchandise: {:?}",merchandises[1])
-    //merchandises_info.merchandises
-    merchandise_dto.merchandises
+    CombineList {
+        merchandises,
+        combines,
+        minimum_threshold,
+        discount,
+    }
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet,max_discount])
+        .invoke_handler(tauri::generate_handler![
+            max_discount])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { For, createResource, createSignal } from "solid-js";
+import { For, Show, createResource, createSignal } from "solid-js";
 import { createStore } from "solid-js/store";
-import logo from "./assets/logo.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import { create } from "domain";
@@ -10,9 +9,28 @@ interface Merchandise {
   price: number,
   amount: number,
 }
+interface MerchandiseRow {
+  merchandise: Merchandise,
+  inModifiedMode: boolean,
+}
 interface MerchandiseDTO {
   merchandises: Merchandise[],
-  minum_threshold: number,
+  minimum_threshold: number,
+  discount: number,
+}
+interface Combine {
+  total_amount:number,
+  price: number,
+  discount_price: number,
+  quantities_combine: number[],
+  cost_performance_ratio: number,
+  cost_per_unit: number,
+}
+interface CombineList {
+  merchandises: Merchandise[],
+  combines: Combine[],
+  minimum_threshold: number,
+  discount: number,
 }
 function App() {
   let formData = {
@@ -22,42 +40,78 @@ function App() {
     minimum_threshold_price: document.createElement('input'),
     discount: document.createElement('input'),
   };
-  const [greetMsg, setGreetMsg] = createSignal("");
-  const [candidateMerchandise, setcandidateMerchandise] = createStore<Merchandise[]>([]);
-  const [name, setName] = createSignal("");
-  const [merchandise, setmerchandise] = createStore<Merchandise[]>([]);
-
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name: name() }));
-  }
+  const [candidateMerchandise, setCandidateMerchandise] = createStore<MerchandiseRow[]>([]);
+  const [combineList, setCombineList] = createStore<CombineList>({
+    merchandises: [], combines: [], minimum_threshold: 0, discount: 0
+  });
 
   const caculateDiscount = async () => {
     const dto: MerchandiseDTO = {
-      merchandises: candidateMerchandise,
-      minum_threshold:parseFloat(formData.minimum_threshold_price.value),
+      merchandises: candidateMerchandise.map(r => r.merchandise),
+      minimum_threshold: parseFloat(formData.minimum_threshold_price.value),
+      discount: parseFloat(formData.discount.value),
     }
-    const mer = await invoke<Merchandise[]>("max_discount", { merchandiseDto: dto });
-    setmerchandise(mer);
-    return mer;
+    const combines = await invoke<CombineList>("max_discount", { merchandiseDto: dto });
+    setCombineList(combines);
   }
 
+  const clearMerchandise = () => {
+    setCandidateMerchandise([]);
+  }
 
   const getMerchandiseInfo = () => {
     return {
-      name: formData.name.value,
-      price: parseInt(formData.price.value),
-      amount: parseInt(formData.amount.value),
+      merchandise: {
+        name: formData.name.value,
+        price: parseInt(formData.price.value),
+        amount: parseInt(formData.amount.value),
+      },
+      inModifiedMode: false
     }
   }
   const enterMerchandise = () => {
-    // const newMerchandise: Merchandise = {
-    //   name: name(),
-    //   price: price(),
-    //   amount: amount(),
-    // }
-    setcandidateMerchandise(old => [...old, getMerchandiseInfo()])
+    setCandidateMerchandise(old => [...old, getMerchandiseInfo()])
+  }
+  const deleteRow = (index:number)=>{
+    setCandidateMerchandise(
+      rows=>rows.filter((r,i)=>i !==index)
+    )
+  }
+  const saveModified = (merchandise:Merchandise,index:number)=>{
+    setCandidateMerchandise(
+      index,
+      _=>{return {merchandise,inModifiedMode:false};},
+    )
+  }
+  const openModified = (index:number)=>{
+    setCandidateMerchandise(
+      index,
+      old=>{return {...old,inModifiedMode : true}},
+    )
+  }
+  function ModifiedRow(props:{row: MerchandiseRow ,index:number}) {
+    const {row,index} = props;
+    const inputData ={
+      name: document.createElement('input'),
+      price:document.createElement('input'),
+      amount:document.createElement('input'),
+    }
+    const getModidfied = ()=>{
+      const modified:Merchandise ={
+        name:inputData.name.value,
+        price:parseFloat(inputData.price.value),
+        amount:parseFloat(inputData.amount.value),
+      }
+      return modified;
+    }
+    return (<div class="grid grid-cols-6 gap-4 w-3/4">
+      <input ref={inputData.name}  value={row.merchandise.name} ></input>
+      <input ref={inputData.price} value={row.merchandise.price}></input>
+      <input ref={inputData.amount} value={row.merchandise.amount}></input>
+      <p>{row.merchandise.amount /row.merchandise.price}</p>
+      <p>{row.merchandise.price /row.merchandise.amount}</p>
+      <button onclick={()=>saveModified(getModidfied(),index)}>Save</button>
+    </div>)
   }
 
   return (
@@ -66,26 +120,24 @@ function App() {
       <div class="flex justify-center">
         <input
           ref={formData!.name}
-          //onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter merchandise name..."
+          placeholder="輸入商品名稱..."
         />
         <input
           ref={formData!.price}
-          //onChange={(e) => setPrice(parseInt(e.currentTarget.value))}
-          placeholder="Enter merchandise amount..."
+          placeholder="輸入價格..."
         />
         <input
           ref={formData!.amount}
-          //onChange={(e) => setAmount(parseInt(e.currentTarget.value))}
-          placeholder="Enter merchandise price..."
+          placeholder="輸入總量..."
         />
 
         <button onClick={enterMerchandise} type="button">Enter</button>
         <button onClick={caculateDiscount} type="button">Caculate</button>
+        <button onClick={clearMerchandise} type="button">Clear</button>
 
       </div>
       <div class="flex justify-center">
-      <input
+        <input
           ref={formData!.minimum_threshold_price}
           placeholder="輸入最低限度金額..."
         />
@@ -94,27 +146,76 @@ function App() {
           placeholder="輸入折扣金額..."
         />
       </div>
-      <div class="bg-red-500"><p>i am here{formData.name.value}</p></div>
       <div class="flex justify-center flex-wrap">
+      <div class="grid grid-cols-7 gap-4 w-3/4">
+        <p>商品名稱</p>
+        <p>價格</p>
+        <p>數量</p>
+        <p>CP值</p>
+        <p>$/unit</p>
+      </div>
+      <div class="w-3/4">
         <For each={candidateMerchandise}>
-          {(merchandise) => (<div class="grid grid-cols-3 gap-4 w-3/4">
-            <p>{merchandise.name}</p>
-            <p>{merchandise.price}</p>
-            <p>{merchandise.amount}</p>
+          {(row,index) => (
+            <Show when={!row.inModifiedMode}
+              fallback={<ModifiedRow row={row} index={index()} />}
+            >
+              <div class="grid grid-cols-7 gap-4 w-3/4">
+                <p>{row.merchandise.name}</p>
+                <p>{row.merchandise.price}</p>
+                <p>{row.merchandise.amount}</p>
+                <p>{row.merchandise.amount / row.merchandise.price}</p>
+                <p>{row.merchandise.price / row.merchandise.amount}</p>
+                <button onclick={()=>openModified(index())}>Modified</button>
+                <button onclick={()=>deleteRow(index())}>Delete</button>
+              </div>
+            </Show>
+          )
+          }
+        </For>
+      </div>
+      </div>
+      <div class="flex justify-around">
+        <div class="flex justify-around w-1/2">
+        <For each={combineList.merchandises}>
+          {(merchandise) => (
+            <p class="text-center">{merchandise.name}</p>
+          )
+          }
+        </For>
+        </div>
+        <div class="flex justify-around w-1/3">
+            <p class="text-center">總量</p>
+            <p class="text-center">原價</p>
+            <p class="text-center">折扣後價格</p>
+            <p class="text-center">CP值</p>
+            <p class="text-center">/Unit</p>
+            </div>
+      </div>
+      <div >
+        <For each={combineList.combines}>
+          {(combine) => (
+          <div class="flex justify-around">
+          <div class="flex justify-around w-1/2">
+            <For each={combine.quantities_combine}>
+              {(quantity) => (
+                <p class="text-center">{quantity}</p>
+              )
+              }
+            </For>
+            </div>
+            <div class="flex justify-around w-1/3">
+            <p class="text-center">{combine.total_amount}</p>
+            <p class="text-center">{combine.price}</p>
+            <p class="text-center">{combine.discount_price}</p>
+            <p class="text-center">{combine.cost_performance_ratio.toFixed(4)}</p>
+            <p class="text-center">{combine.cost_per_unit.toFixed(4)}</p>
+            </div>
           </div>
           )
           }
         </For>
       </div>
-      <For each={merchandise}>
-        {(merchandise) => (<div class="grid grid-cols-3 gap-4 w-3/4">
-          <p>{merchandise.name}</p>
-          <p>{merchandise.price}</p>
-          <p>{merchandise.amount}</p>
-        </div>
-        )
-        }
-      </For>
     </>
   );
 }
